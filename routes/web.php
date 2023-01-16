6b708885faa2cf6ffd19013cf591d07875c46f7e
Arrcontender <dramman4@gmail.com>
2023-01-16 20:37:48 +0300

    started to dev api

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', function() {
-    if (DB::connection()->getDatabaseName())  {
-      dd('Есть контакт!');
-    } else {
-      return 'Соединения нет';
-    }
-});
