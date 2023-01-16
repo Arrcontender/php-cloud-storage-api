c259c0ae0635829b612cf4ef2b70bf509ad322f7
Arrcontender <dramman4@gmail.com>
2023-01-16 18:51:36 +0300

    fixed schemas

--- database/migrations/2023_01_16_152840_create_files_table.php
+++ database/migrations/2023_01_16_152840_create_files_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('folder_id')->constrained();
             $table->timestamps();
         });
     }
